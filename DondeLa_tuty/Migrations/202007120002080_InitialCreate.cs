a0275bae53f7d4fdddac91233d9f7833aef927fa
criistobaalr <criistobaalr@gmail.com>
2020-07-11 20:30:15 -0400

    migraciones [CR]

---
+++ DondeLa_tuty/Migrations/202007120002080_InitialCreate.cs
@@ -0,0 +1,118 @@
+﻿namespace shoppingstore.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Carts",
+                c => new
+                    {
+                        RecordId = c.Int(nullable: false, identity: true),
+                        CartId = c.String(),
+                        ItemId = c.Int(nullable: false),
+                        Count = c.Int(nullable: false),
+                        DateCreated = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.RecordId)
+                .ForeignKey("dbo.Items", t => t.ItemId, cascadeDelete: true)
+                .Index(t => t.ItemId);
+            
+            CreateTable(
+                "dbo.Items",
+                c => new
+                    {
+                        ItemId = c.Int(nullable: false, identity: true),
+                        CategoryId = c.Int(nullable: false),
+                        ProducerID = c.Int(nullable: false),
+                        Titulo = c.String(nullable: false, maxLength: 160),
+                        Precio = c.Int(nullable: false),
+                        ItemArtUrl = c.String(maxLength: 1024),
+                    })
+                .PrimaryKey(t => t.ItemId)
+                .ForeignKey("dbo.Categories", t => t.CategoryId, cascadeDelete: true)
+                .ForeignKey("dbo.Producers", t => t.ProducerID, cascadeDelete: true)
+                .Index(t => t.CategoryId)
+                .Index(t => t.ProducerID);
+            
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        CategoryId = c.Int(nullable: false, identity: true),
+                        Nombre = c.String(),
+                        Descripcion = c.String(),
+                    })
+                .PrimaryKey(t => t.CategoryId);
+            
+            CreateTable(
+                "dbo.Producers",
+                c => new
+                    {
+                        ProducerId = c.Int(nullable: false, identity: true),
+                        Nombre = c.String(),
+                    })
+                .PrimaryKey(t => t.ProducerId);
+            
+            CreateTable(
+                "dbo.OrderDetails",
+                c => new
+                    {
+                        OrderDetailId = c.Int(nullable: false, identity: true),
+                        OrderId = c.Int(nullable: false),
+                        ItemId = c.Int(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                        UnitPrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.OrderDetailId)
+                .ForeignKey("dbo.Items", t => t.ItemId, cascadeDelete: true)
+                .ForeignKey("dbo.Orders", t => t.OrderId, cascadeDelete: true)
+                .Index(t => t.OrderId)
+                .Index(t => t.ItemId);
+            
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        OrderId = c.Int(nullable: false, identity: true),
+                        Username = c.String(),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Address = c.String(),
+                        City = c.String(),
+                        State = c.String(),
+                        PostalCode = c.String(),
+                        Country = c.String(),
+                        Phone = c.String(),
+                        Email = c.String(),
+                        Total = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        OrderDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.OrderId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.OrderDetails", "OrderId", "dbo.Orders");
+            DropForeignKey("dbo.OrderDetails", "ItemId", "dbo.Items");
+            DropForeignKey("dbo.Carts", "ItemId", "dbo.Items");
+            DropForeignKey("dbo.Items", "ProducerID", "dbo.Producers");
+            DropForeignKey("dbo.Items", "CategoryId", "dbo.Categories");
+            DropIndex("dbo.OrderDetails", new[] { "ItemId" });
+            DropIndex("dbo.OrderDetails", new[] { "OrderId" });
+            DropIndex("dbo.Items", new[] { "ProducerID" });
+            DropIndex("dbo.Items", new[] { "CategoryId" });
+            DropIndex("dbo.Carts", new[] { "ItemId" });
+            DropTable("dbo.Orders");
+            DropTable("dbo.OrderDetails");
+            DropTable("dbo.Producers");
+            DropTable("dbo.Categories");
+            DropTable("dbo.Items");
+            DropTable("dbo.Carts");
+        }
+    }
+}
